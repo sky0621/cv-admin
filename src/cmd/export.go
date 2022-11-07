81caa72950d74eb631df205e3438deb923b3f99d
sky0621 <sky14work@gmail.com>
2022-11-07 09:50:18 +0900

    continue

--- src/cmd/export.go
+++ src/cmd/export.go
@@ -1,14 +1,21 @@
 /*
 Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 
 	"github.com/spf13/cobra"
 )
+
+// 厳密にはプロトコル＋FQDNだけど。
+var requestFQDN = "http://localhost:8080"
+
+var targetUserID = "26"
 
 // exportCmd represents the export command
 var exportCmd = &cobra.Command{
@@ -21,7 +28,29 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("export called")
+		cli := http.DefaultClient
+		/*
+		 * user attribute
+		 */
+		req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/users/%s/attribute", requestFQDN, targetUserID), nil)
+		if err != nil {
+			fmt.Printf("[user attribute][NewRequest] %v\n", err)
+			os.Exit(1)
+		}
+
+		res, err := cli.Do(req)
+		if err != nil {
+			fmt.Printf("[user attribute][Do] %v\n", err)
+			os.Exit(1)
+		}
+
+		resBody, err := io.ReadAll(res.Body)
+		if err != nil {
+			fmt.Printf("[user attribute][ReadAll] %v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("%s\n", resBody)
 	},
 }
 
