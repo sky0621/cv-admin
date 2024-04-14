9aa988fe98b4d71af922248c4cf78f7890700134
sky0621 <sky14work@gmail.com>
2024-04-14 10:44:14 +0900

    -

--- src/rest/get_skill_tags_by_skillTagId.go
+++ src/rest/get_skill_tags_by_skillTagId.go
@@ -4,14 +4,12 @@ import (
 	"context"
 
 	"github.com/sky0621/cv-admin/src/ent"
-
-	"github.com/sky0621/cv-admin/src/ent/skilltag"
 )
 
 // GetSkilltagsBySkillTagId 指定スキルタグ取得
 // (GET /skilltags/{bySkillTagId})
 func (s *strictServerImpl) GetSkilltagsBySkillTagId(ctx context.Context, request GetSkilltagsBySkillTagIdRequestObject) (GetSkilltagsBySkillTagIdResponseObject, error) {
-	entSkillTag, err := s.dbClient.SkillTag.Query().Where(skilltag.ID(request.BySkillTagId)).Only(ctx)
+	entSkillTag, err := s.dbClient.SkillTag.Get(ctx, request.BySkillTagId)
 	if err != nil {
 		if ent.IsNotFound(err) {
 			return GetSkilltagsBySkillTagId404JSONResponse{n404("skillTag is none")}, nil
